1e8070a48fcee5af1b568c907ebfae50e32e4eda
fahmi <fahmizhd52@gmail.com>
2024-06-21 13:56:58 +0700

    without db api done

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
+# from sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flask-restful'
+db = SQLAlchemy(app)
 
 # Basic get method
 class HelloWorld(Resource):
@@ -11,7 +15,8 @@ class HelloWorld(Resource):
             "data" : "Hello World",
             "status" : "good",
             "message" : "yes"
-                }
+        }
+    
     def post(self) :
         return {
             "data" : "Hellow World",
@@ -51,6 +56,56 @@ class PassingFromVar(Resource):
         return names[name]
 
 api.add_resource(PassingFromVar, "/var/<string:name>")
+
+# Get the data from url
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the Video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the Video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the Video", required=True)
+
+videos = {}
+
+def VideoNotExist(video_id) :
+    if video_id not in videos:
+        abort(404, message="Video id is not exist...")
+
+def VideoisExist(video_id) :
+    if video_id in videos:
+        abort(409, message="Video already exist... ")
+
+class Video(Resource):
+    def get(self, video_id) :
+        VideoNotExist(video_id)
+        return videos[video_id]
+    
+    def put(self, video_id) :
+        VideoisExist(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+    
+    def delete(self, video_id) :
+        VideoNotExist(video_id)
+        del videos[video_id]
+        return '', 204
+
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+# Connecting into local database
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    like = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+# Create all database tables
+with app.app_context():
+    db.create_all()
+    
 
 # Running the flask app
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -8,7 +8,24 @@ response2 = requests.post(BASE + "helloworld")
 response3 = requests.get(BASE + "params/fahmi/1")
 response4 = requests.get(BASE + "var/not-fahmi")
 
+# response5 = requests.put(BASE + "video/1", {"likes":10, "name":"intro", "views":1000})
+input()
+response6 = requests.get(BASE + "video/1")
+
+data = [{"likes":214, "name":"intro", "views":3234},
+        {"likes":554, "name":"main", "views":4356},
+        {"likes":32, "name":"end", "views":34}]
+
+for i in range(len(data)):
+    response5 = requests.put(BASE + "video/" + str(i), data[i])
+    print(response5.json())
+
+input()
+response7 = requests.delete(BASE + "video/0")
+
 print(response1.json())
 print(response2.json())
 print(response3.json())
 print(response4.json())
+print(response6.json())
+print(response7)
